58b18404848b89c1050787314c90ab7f01ebf621
Damien Antipa <dantipa@adobe.com>
2014-06-19 15:18:28 +0200

    add bounce easing

--- src/scrollview.js
+++ src/scrollview.js
@@ -9,6 +9,9 @@
         },
         back: {
             transition: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)'
+        },
+        bounce: {
+            transition: 'cubic-bezier(0,0,.5,1.2)'
         }
     };
 
